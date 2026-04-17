96a41f1d3f86b2d180a61d56c69eeed7a35550c2
Gloria <mcalping@ccsdbraves.org>
2026-04-17 18:51:48 -0400

    Roller Bed Backward

--- src/main/java/frc/robot/commands/ShootWithIndexer.java
+++ src/main/java/frc/robot/commands/ShootWithIndexer.java
@@ -4,19 +4,30 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Shooter;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ShootWithIndexer extends Command {
   private Shooter shooter;
   private Indexer indexer;
+  private Timer timer;
+
+  private enum RollerState {
+    STOPPED,
+    FORWARD,
+    BACKWARD
+  }
+
+  private RollerState rollerState;
 
   public ShootWithIndexer(Shooter shooter, Indexer indexer) {
     this.shooter = shooter;
     this.indexer = indexer;
+    timer = new Timer();
     // this.turret = turret;
     addRequirements(this.indexer);
   }
@@ -25,20 +36,44 @@ public class ShootWithIndexer extends Command {
   @Override
   public void initialize() {
     indexer.stopIndexer();
+    timer.start();
+    rollerState = RollerState.STOPPED;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-      if (shooter.runIndexer()) {
+    if (shooter.runIndexer()) {
+      switch (rollerState) {
+        case STOPPED:
           indexer.indexerForward();
-          SmartDashboard.putString("Indexer Default Command", "forward");
-        }
-        else {
-          indexer.stopIndexer();
-          SmartDashboard.putString("Indexer Default Command", "stop");
-        }
+          rollerState = RollerState.FORWARD;
+          timer.reset();
+          break;
+        case FORWARD:
+          if (timer.hasElapsed(2.0)) {
+            indexer.indexerRollBackward();
+            timer.reset();
+            rollerState = RollerState.BACKWARD;
+          }
+          break;
+        case BACKWARD:
+          if (timer.hasElapsed(0.5)) {
+            indexer.indexerForward();
+            timer.reset();
+            rollerState = RollerState.FORWARD;
+          }
+          break;
+        default:
+          break;
       }
+      SmartDashboard.putString("Indexer Default Command", rollerState.name());
+    } else {
+      indexer.stopIndexer();
+      rollerState = RollerState.STOPPED;
+      SmartDashboard.putString("Indexer Default Command", rollerState.name());
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
